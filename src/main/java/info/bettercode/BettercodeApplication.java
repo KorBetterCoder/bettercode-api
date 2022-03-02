6004a54d602354a3da90c0a12163a36c5a0c8eff
galashow7@gmail.com <galashow7@helixtech.co.kr>
2022-03-02 15:44:54 +0900

    웹이니셜 라이즈로 변경

---
+++ src/main/java/info/bettercode/BettercodeApplication.java
@@ -0,0 +1,13 @@
+package info.bettercode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettercodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettercodeApplication.class, args);
+	}
+
+}
